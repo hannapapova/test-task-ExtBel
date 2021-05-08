c4e33ab6241fcc74c6113fa55bf879b3ec069baa
Hanna Papova <gannapapova@gmail.com>
2021-05-08 16:56:59 +0300

    Add activity and fragment views

--- app/src/main/java/com/hannapapova/testtaskextbel/MainActivity.kt
+++ app/src/main/java/com/hannapapova/testtaskextbel/MainActivity.kt
@@ -1,7 +1,7 @@
-package com.hannapapova.testtaskextbel
+    package com.hannapapova.testtaskextbel
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
